7fddf7244e74d82c59998ec0e40633995974a287
Calvin Tan <CalvinwsTan37@gmail.com>
2022-01-17 22:55:42 +0000

    aligned input boxes

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function compute()
 
     document.getElementById("result").innerHTML="If you deposit "+principal+ 
     ",<br\>at an interest rate of "+rate+"%.<br\>You will receive an amount of "+interest+
-    ",<br\>in the year "+year+"<br\>";
+    ",<br\>in the year "+year+"<br\><br\>";
     
 }
         
