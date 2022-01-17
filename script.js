9b22373c995d0f3e186f60caa468fb45970c3fc4
Calvin Tan <CalvinwsTan37@gmail.com>
2022-01-17 23:17:28 +0000

    Fixed a few stuff

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
 
 function compute()
@@ -18,9 +18,10 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+ 
-    ",<br\>at an interest rate of "+rate+"%.<br\>You will receive an amount of "+interest+
-    ",<br\>in the year "+year+"<br\><br\>";
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+ 
+    "</mark>,<br\>at an interest rate of <mark>"+rate+
+    "%</mark>.<br\>You will receive an amount of <mark>"+interest+
+    "</mark>,<br\>in the year <mark>"+year+"</mark><br\><br\>";
     
 }
         
